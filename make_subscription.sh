1af09aeb5c3722692c2dcdec1ec8a45154fcdb09
nottombrown <nottombrown@gmail.com>
2011-08-27 17:01:51 -0700

    added subscription

---
+++ make_subscription.sh
@@ -0,0 +1,7 @@
+curl -F 'client_id=164413c65ebe413ebba621192521154e' \
+     -F 'client_secret=026f82f7a7d54e968ff1a77fb3a48d14' \
+     -F 'object=tag' \
+     -F 'aspect=media' \
+     -F 'object_id=hug' \
+     -F 'callback_url=http://hugs.nottombrown.com/add_hug' \
+     https://api.instagram.com/v1/subscriptions/

177acdd2ea125a13448f94ec5a0bafa351907335
nottombrown <nottombrown@gmail.com>
2011-08-30 21:06:30 -0700

    about to port to backbone/coffeescript

--- static/main.js
+++ static/main.js
@@ -1,24 +1,12 @@
 var shortcodes
 var hugTemplate
 var panelTemplate   
-var _nextImage;   
-    
-var nextImage = function(){  
-    // Get next image, prepare the subsequent one
-    toReturn = _nextImage.show();
-    _nextImage = "test"
-    return toReturn;
-};   
- 
- 
-var nextTestImageURL = function(){      
-    // we keep a buffer of 50 total hugs
-    // replenish the buffer when it goes below 25
-    var _index = 0; 
-    
-    return function(){                                           
-        var url = "http://instagr.am/p/"+shortcodes[_index]+"/media/?size=m";
-        _index++;
+
+var nextTestImageURL = function(){                    
+    var _index = 0;    
+    return function(){       
+        var _index = Math.floor(Math.random() * shortcodes.length )  
+        var url = "http://instagr.am/p/"+shortcodes[_index]+"/media/?size=m";     
         return url
     }
 }();   
@@ -35,49 +23,68 @@ var initialize = function(){
                       
     for (i=1;i<=rows;i++){
         for (j=1;j<=cols;j++){     
-            newHug();    
+            initializeNewHug();    
         
         }
-    }                  
+    } 
+    
+    panels = $("#container .panel")
+    
+    setTimeout(function(){
+        $.each(panels, function(i, panel){  
+            
+            addInstagram($(panel).find(".back .instagram"));   
+            // Bind to on toggle      
+         
+        });                  
+    }, 2000);
+                            
 }     
+  
+function addInstagram(elt){
+    $(elt).attr("src", nextTestImageURL());
+}
 
-function newHug(newShortcode){            
+function initializeNewHug(newShortcode){            
     
     var panel = panelTemplate.clone().show();    
-    var url = nextTestImageURL() 
     
-    panel.find(".instagram").attr("src", url) 
+    addInstagram($(panel).find(".front .instagram"));
+    panel.appendTo($("#container"));  
     
-    console.log(panel)
-    // Bind to on toggle              
-    panel.toggle(function(){   
-        that = this;
-         
-        // Add new image
-        // console.log($(this));
-        // $(this).find(".back").append(nextImage());     
-        
-        // Flip the tile   
-		$(this).addClass('flipping').addClass('flip');
+    
+       
+
+   $(panel).toggle(function(){  
+       // Flip to back  
+
+       // Flip the tile   
+		$(this).addClass('flipping').addClass('flip');  
+		that = this;    
 		setTimeout(function(){
-                 $(that).removeClass("flipping");
-           },400);         
-	},function(){
+                $(that).removeClass("flipping"); 
+                // addInstagram($(panel).find(".front .instagram")) 
+          },400);         
+	},function(){ 
+	    // Flip to front
+
+
 		$(this).addClass('flipping').removeClass('flip');  
 		that = this;   
 		setTimeout(function(){
-                 $(that).removeClass("flipping");
-           },400);   
-	});
-    panel.appendTo($("#container"))
+                $(that).removeClass("flipping");   
+                // $(panel).find(".back .instagram").attr("src", nextTestImageURL())  
+          },400);   
+	});    
+    
+    
 }  
 
 $(function(){ 
     // set up click/tap panels   
     $.get("get_hugs", function(data){  
         shortcodes = data;  
-        initialize()   
-        // _nextImage = $("body").append("<img class='instagram' src='" + nextTestImageURL() + "'>"); 
+        initialize();                                                                                    
        
     });
 });
--- templates/hugs.mako
+++ templates/hugs.mako
@@ -6,8 +6,8 @@
       <div class="hug-container front">
         <img class="instagram" src="">   
       </div> 
-      <div class="back">              
-        <!-- Placeholder for whatever we put on the back    -->
+      <div class="hug-container back">       
+        <img class="instagram" src="">                            
       </div>    
     </div>
   </div>  
